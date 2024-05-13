351dda8939e12470cde3b818e9c769b2f1930345
Mubarak Mohamed <76488135+mbr-ctrl@users.noreply.github.com>
2024-05-13 21:57:56 +0100

    Ajout de google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "./_components/GoogleAnalytics";
 import { ThemeProvider } from "./_components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
+        <GoogleAnalytics />
         <body className="container mx-auto-screen">
           <ThemeProvider
             attribute="class"
